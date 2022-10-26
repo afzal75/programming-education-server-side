df69753ef1fb77778dd9303b5b79bee5aad299cb
afzal75 <afzalbhuiyan16@gmail.com>
2022-10-26 20:46:54 +0600

    courses name api done left side bar name show

--- index.js
+++ index.js
@@ -3,12 +3,18 @@ const app = express();
 const cors = require('cors');
 const port = process.env.PORT || 5000;
 
-app.use(cors(cors));
+app.use(cors());
+
+const subject = require('./data/subject.json');
 
 app.get('/', (req, res) => {
     res.send('Subject API running');
 });
 
+app.get('/subject-courses', (req, res) => {
+    res.send(subject);
+})
+
 app.listen(port, () => {
-    console.log(`Programming Education Server running on port, ${port}`);
+    console.log('Programming Education Server running on port', port);
 })
