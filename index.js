94767bff07d5f5b90ef205a71cf757bf2bb15f9a
afzal75 <afzalbhuiyan16@gmail.com>
2022-10-28 05:02:13 +0600

    project completed

--- index.js
+++ index.js
@@ -5,16 +5,12 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 
-const subject = require('./data/subject.json');
 const courses = require('./data/courses.json');
 
 app.get('/', (req, res) => {
     res.send('Subject API running');
 });
 
-app.get('/subject-courses', (req, res) => {
-    res.send(subject);
-});
 
 app.get('/courses-categories', (req, res) => {
     res.send(courses);
